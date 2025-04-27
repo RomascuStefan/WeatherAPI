e60fbd049b2adc97dec20c383822d5855d009b14
Stefan <stefanromascu030405@gmail.com>
2025-04-27 19:40:01 +0300

    Added BCryptPasswordEncoder bean

--- src/main/java/com/example/demo/Extra/Config.java
+++ src/main/java/com/example/demo/Extra/Config.java
@@ -2,6 +2,7 @@ package com.example.demo.Extra;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,6 +11,11 @@ public class Config {
     @Bean
     public RestTemplate RestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }

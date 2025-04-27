5ac4d175e36fc4d839fe37f77cd62e1c2d8950dc
Stefan <stefanromascu030405@gmail.com>
2025-04-27 20:12:38 +0300

    added new function

--- src/main/java/com/example/demo/Repository/UserProfileDAO.java
+++ src/main/java/com/example/demo/Repository/UserProfileDAO.java
@@ -1,9 +1,14 @@
 package com.example.demo.Repository;
 
+import com.example.demo.Model.User;
 import com.example.demo.Model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserProfileDAO  extends JpaRepository<UserProfile, Long> {
+    Optional<UserProfile> findByEmail(String email);
+
 }

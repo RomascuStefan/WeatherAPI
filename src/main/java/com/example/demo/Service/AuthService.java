bd20f5e50ecb62321dedea55c89eae710257e147
Stefan <stefanromascu030405@gmail.com>
2025-04-27 19:39:10 +0300

    hot fix

--- src/main/java/com/example/demo/Service/AuthService.java
+++ src/main/java/com/example/demo/Service/AuthService.java
@@ -1,11 +1,11 @@
 package com.example.demo.Service;
 
 import com.example.demo.DTO.AuthDTO;
-import com.example.demo.Exception.InvalidCredentialsException;
+import com.example.demo.Advice.Exception.InvalidCredentialsException;
 import com.example.demo.Extra.HelperFunctions;
-import com.example.demo.Extra.JwtUtil;
 import com.example.demo.Model.User;
 import com.example.demo.Repository.UserDAO;
+import com.example.demo.Security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,16 +17,16 @@ public class AuthService {
     private final JwtUtil jwtUtil;
 
     @Autowired
-    public AuthService(UserDAO userDAO, JwtUtil jwtUtil) {
+    public AuthService(UserDAO userDAO,  JwtUtil jwtUtil) {
         this.userDAO = userDAO;
         this.jwtUtil = jwtUtil;
     }
 
-    public String login(AuthDTO auth) {
-        User user = userDAO.findByUsername(auth.getUsername())
-                .orElseThrow(InvalidCredentialsException::new);
 
-        if (!HelperFunctions.getHashString(auth.getPassword()).equals(user.getPassword())) {
+    public String login(AuthDTO auth) {
+        User user = userDAO.findByUsername(auth.getUsername()).orElseThrow(InvalidCredentialsException::new);
+
+        if (!HelperFunctions.passwordsMatch(auth.getPassword(), user.getPassword())) {
             throw new InvalidCredentialsException();
         }
 

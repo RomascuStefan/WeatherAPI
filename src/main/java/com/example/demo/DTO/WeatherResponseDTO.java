643b71b6fb9dfcbe071cf42466414f82344d2a54
Stefan <stefanromascu030405@gmail.com>
2025-04-28 13:46:04 +0300

    added AQI response in DTO

--- src/main/java/com/example/demo/DTO/WeatherResponseDTO.java
+++ src/main/java/com/example/demo/DTO/WeatherResponseDTO.java
@@ -1,14 +1,19 @@
 package com.example.demo.DTO;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Map;
+
 @AllArgsConstructor
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class WeatherResponseDTO {
     private String name;
     private String country;
     private String localtime;
     private double tempC;
     private double windKph;
+    private Map<String,Object> airQuality;
 }

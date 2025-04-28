b01c2a5ab9d4b3effa27efb05aa45d3d7c1c2cd5
Stefan <stefanromascu030405@gmail.com>
2025-04-28 10:54:59 +0300

    removed always true if statement

--- src/main/java/com/example/demo/Service/WeatherService.java
+++ src/main/java/com/example/demo/Service/WeatherService.java
@@ -1,5 +1,7 @@
 package com.example.demo.Service;
 
+import com.example.demo.DTO.Mapper.WeatherResponseMapper;
+import com.example.demo.DTO.WeatherResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,28 +10,18 @@ import java.util.Map;
 
 @Service
 public class WeatherService {
-
     private final RestTemplate restTemplate;
-
 
     @Autowired
     public WeatherService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
-    public Map<Object, Object> getWeatherStatus(String apiKey, int lat, int lon, Boolean q, Boolean aqi) {
+    public WeatherResponseDTO getWeatherStatus(String apiKey, int lat, int lon, Boolean q, Boolean aqi) {
         String url = "http://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + lat + "," + lon;
-        if (q != null) {
-            url += "&include_q=" + (q ? "yes" : "no");
-        }
-        if (aqi != null) {
-            url += "&aqi=" + (aqi ? "yes" : "no");
-        }
-        return restTemplate.getForObject(url, Map.class);
+        url += "&include_q=" + (q ? "yes" : "no");
+        url += "&aqi=" + (aqi ? "yes" : "no");
+
+        return WeatherResponseMapper.toDTO(restTemplate.getForObject(url, Map.class));
     }
-
-
-
-
-
 }

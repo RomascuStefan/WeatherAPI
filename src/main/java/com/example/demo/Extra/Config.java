ee8151e18c8c1ef29df173c67642ee4c4ca10df8
Stefan <stefanromascu030405@gmail.com>
2025-04-28 11:13:49 +0300

    enable aspect proxy

--- src/main/java/com/example/demo/Extra/Config.java
+++ src/main/java/com/example/demo/Extra/Config.java
@@ -2,10 +2,12 @@ package com.example.demo.Extra;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class Config {
 
     @Bean

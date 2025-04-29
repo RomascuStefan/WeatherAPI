8976435db77c3a667f3ee3e95e01a63c5e5cf596
Stefan <stefanromascu030405@gmail.com>
2025-04-29 20:55:43 +0300

    Added @Query

--- src/main/java/com/example/demo/Repository/UserDAO.java
+++ src/main/java/com/example/demo/Repository/UserDAO.java
@@ -1,13 +1,14 @@
 package com.example.demo.Repository;
 
-
 import com.example.demo.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserDAO extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.username = :username")
     Optional<User> findByUsername(String username);
 }
--- src/main/java/com/example/demo/Repository/UserProfileDAO.java
+++ src/main/java/com/example/demo/Repository/UserProfileDAO.java
@@ -1,14 +1,16 @@
 package com.example.demo.Repository;
 
-import com.example.demo.Model.User;
 import com.example.demo.Model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserProfileDAO  extends JpaRepository<UserProfile, Long> {
+
+    @Query("SELECT p FROM UserProfile p WHERE p.email = :email")
     Optional<UserProfile> findByEmail(String email);
 
 }

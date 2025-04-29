6a7afc33de257076524cd954af679ca7c213fa5f
Stefan <stefanromascu030405@gmail.com>
2025-04-29 17:47:29 +0300

    added logs interceptor

--- src/main/java/com/example/demo/Extra/WebConfig.java
+++ src/main/java/com/example/demo/Extra/WebConfig.java
@@ -1,5 +1,7 @@
-package com.example.demo.Security;
+package com.example.demo.Extra;
 
+import com.example.demo.Logs.LoggingInterceptor;
+import com.example.demo.Security.JwtInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
@@ -8,15 +10,20 @@ import org.springframework.web.servlet.config.annotation.*;
 public class WebConfig implements WebMvcConfigurer {
 
     private final JwtInterceptor jwtInterceptor;
+    private final LoggingInterceptor loggingInterceptor;
 
     @Autowired
-    public WebConfig(JwtInterceptor jwtInterceptor) {
+    public WebConfig(JwtInterceptor jwtInterceptor, LoggingInterceptor loggingInterceptor) {
         this.jwtInterceptor = jwtInterceptor;
+        this.loggingInterceptor = loggingInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/api/**");
+
+        registry.addInterceptor(loggingInterceptor)
                 .addPathPatterns("/api/**");
     }
 
